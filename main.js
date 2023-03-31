bcbb1dba82b9efc87f11ccfc874d95977d038cf6
wgm22ar <wgm22ar@gmail.com>
2023-03-31 17:20:17 -0300

    soluciones de bugs

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 alert(" Bienvenidos a Gennaro s Pizzas & Empanadas ");
 let nombreUsuario = prompt("Ingrese el nombre de Ususario: ");
 let direccionEnvio = prompt("Por favor ingrese la dirirección de envio: ");
+
 const producto = prompt(
   "Quiere realizar un pedido de \n a) Pizzas \n b) Empanadas \n c) Bedidas "
 );
+
 let cantidadPedida = 0;
+let netoApagar = 0;
+let precioProducto = 0;
+let precioAdicional = 0;
+let totalAdicional = 0;
+let totalApagar = 0;
+
 const precioPizza = 100;
 const precioEmpanada = 20;
 const precioBebida = 50;
+
 if (producto == "pizzas") {
   totalProducto = Number(prompt("Ingrese la cantidad deseada :"));
-  let precioProducto = precioPizza;
-  netoApagar = totalProducto * precioProducto;
+  netoApagar = totalProducto * precioPizza;
   console.log(
     `Ud pidio ${totalProducto} ${producto}.El monto a abonar es de $ ${netoApagar}`
   );
   alert(`El monto a abonar es de $ ${netoApagar}`);
 } else if (producto == "empanadas") {
   totalProducto = Number(prompt("Ingrese la cantidad deseada :"));
-  let precioProducto = precioEmpanada;
-  netoApagar = totalProducto * precioProducto;
+  netoApagar = totalProducto * precioEmpanada;
   console.log(
     `Ud pidio ${totalProducto} ${producto}.El monto a abonar es de $ ${netoApagar}`
   );
   alert(`El monto a abonar es de $ ${netoApagar}`);
 } else if (producto == "bebidas") {
   totalProducto = Number(prompt("Ingrese la cantidad deseada :"));
-  let precioProducto = precioBebida;
-  netoApagar = totalProducto * precioProducto;
+  netoApagar = totalProducto * precioBebida;
   console.log(
     `Ud pidio ${totalProducto} ${producto}.El monto a abonar es de $ ${netoApagar}`
   );
@@ -36,43 +42,37 @@ if (producto == "pizzas") {
   alert(
     "No ha ingresado una opcion valida. Por favor ingrese los item en minúsculas"
   );
+
 let agregarPedido = true;
+
 while (agregarPedido) {
-  const precioPizza = 100;
-  const precioEmpanada = 20;
-  const precioBebida = 50;
   productoAdicional = prompt(
     "Que desea agrear \n a) Pizzas \n b) Empanadas \n c) Bedidas "
   );
   if (productoAdicional == "pizzas") {
     cantidadAdicional = Number(prompt("Ingrese la cantidad deseada :"));
-    let precioAdicional = precioPizza;
-    let precioProducto = precioPizza;
-    totalAdicional = precioAdicional * cantidadAdicional;
+
+    totalAdicional = precioPizza * cantidadAdicional;
     console.log(
       `Ud agregó ${cantidadAdicional} ${productoAdicional}. El monto adicional es de $ ${totalAdicional}`
     );
-    totalApagar = netoApagar + totalAdicional;
+    totalApagar += netoApagar + totalAdicional;
     alert(`El monto a abonar es de $ ${totalApagar}`);
   } else if (productoAdicional == "empanadas") {
     cantidadAdicional = Number(prompt("Ingrese la cantidad deseada :"));
-    let precioAdicional = precioEmpanada;
-    let precioProducto = precioEmpanada;
-    totalAdicional = precioAdicional * cantidadAdicional;
+    totalAdicional = precioEmpanada * cantidadAdicional;
     console.log(
       `Ud agregó ${cantidadAdicional} ${productoAdicional}. El monto adicional es de $ ${totalAdicional}`
     );
-    totalApagar = netoApagar + totalAdicional;
+    totalApagar += netoApagar + totalAdicional;
     alert(`El monto a abonar es de $ ${totalApagar}`);
   } else if (productoAdicional == "bebidas") {
     cantidadAdicional = Number(prompt("Ingrese la cantidad deseada :"));
-    let precioAdicional = precioBebida;
-    precioProducto = precioBebida;
-    totalAdicional = precioAdicional * cantidadAdicional;
+    totalAdicional = precioBebida * cantidadAdicional;
     console.log(
       `Ud agregó ${cantidadAdicional} ${productoAdicional}. El monto adicional es de $ ${totalAdicional}`
     );
-    totalApagar = netoApagar + totalAdicional;
+    totalApagar += netoApagar + totalAdicional;
     alert(`El monto a abonar es de $ ${totalApagar}`);
   }
   agregarPedido = confirm("¿Desea agreagar algun producto más?");

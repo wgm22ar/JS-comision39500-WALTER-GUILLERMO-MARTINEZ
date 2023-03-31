9f5d2aac5627a46f1b27d4e0734c0330bd33b0fe
wgm22ar <wgm22ar@gmail.com>
2023-03-31 04:37:15 -0300

    Arreglos varios (falta acumular adicionales)

--- main.js
+++ main.js
@@ -10,23 +10,32 @@ const precioEmpanada = 20;
 const precioBebida = 50;
 if (producto == "pizzas") {
   totalProducto = Number(prompt("Ingrese la cantidad deseada :"));
-  console.log(`Ud pidio ${totalProducto} ${producto}`);
   let precioProducto = precioPizza;
   netoApagar = totalProducto * precioProducto;
+  console.log(
+    `Ud pidio ${totalProducto} ${producto}.El monto a abonar es de $ ${netoApagar}`
+  );
   alert(`El monto a abonar es de $ ${netoApagar}`);
 } else if (producto == "empanadas") {
   totalProducto = Number(prompt("Ingrese la cantidad deseada :"));
-  console.log(`Ud pidio ${totalProducto} ${producto}`);
   let precioProducto = precioEmpanada;
   netoApagar = totalProducto * precioProducto;
+  console.log(
+    `Ud pidio ${totalProducto} ${producto}.El monto a abonar es de $ ${netoApagar}`
+  );
   alert(`El monto a abonar es de $ ${netoApagar}`);
 } else if (producto == "bebidas") {
   totalProducto = Number(prompt("Ingrese la cantidad deseada :"));
-  console.log(`Ud pidio ${totalProducto} ${producto}`);
   let precioProducto = precioBebida;
   netoApagar = totalProducto * precioProducto;
+  console.log(
+    `Ud pidio ${totalProducto} ${producto}.El monto a abonar es de $ ${netoApagar}`
+  );
   alert(`El monto a abonar es de $ ${netoApagar}`);
-} else alert("No ha ingresado una opcion valida");
+} else
+  alert(
+    "No ha ingresado una opcion valida. Por favor ingrese los item en minúsculas"
+  );
 let agregarPedido = true;
 while (agregarPedido) {
   const precioPizza = 100;
@@ -36,29 +45,38 @@ while (agregarPedido) {
     "Que desea agrear \n a) Pizzas \n b) Empanadas \n c) Bedidas "
   );
   if (productoAdicional == "pizzas") {
-    totalAdicional = Number(prompt("Ingrese la cantidad deseada :"));
-    console.log(`Ud agregó ${totalAdicional} ${productoAdicional}`);
+    cantidadAdicional = Number(prompt("Ingrese la cantidad deseada :"));
     let precioAdicional = precioPizza;
     let precioProducto = precioPizza;
-    totalApagar = netoApagar + totalAdicional * precioAdicional;
+    totalAdicional = precioAdicional * cantidadAdicional;
+    console.log(
+      `Ud agregó ${cantidadAdicional} ${productoAdicional}. El monto adicional es de $ ${totalAdicional}`
+    );
+    totalApagar = netoApagar + totalAdicional;
     alert(`El monto a abonar es de $ ${totalApagar}`);
   } else if (productoAdicional == "empanadas") {
-    totalAdicional = Number(prompt("Ingrese la cantidad deseada :"));
-    console.log(`Ud agregó ${totalAdicional} ${productoAdicional}`);
+    cantidadAdicional = Number(prompt("Ingrese la cantidad deseada :"));
     let precioAdicional = precioEmpanada;
     let precioProducto = precioEmpanada;
-    totalApagar = netoApagar + totalAdicional * precioAdicional;
+    totalAdicional = precioAdicional * cantidadAdicional;
+    console.log(
+      `Ud agregó ${cantidadAdicional} ${productoAdicional}. El monto adicional es de $ ${totalAdicional}`
+    );
+    totalApagar = netoApagar + totalAdicional;
     alert(`El monto a abonar es de $ ${totalApagar}`);
   } else if (productoAdicional == "bebidas") {
-    totalAdicional = Number(prompt("Ingrese la cantidad deseada :"));
-    console.log(`Ud agregó ${totalAdicional} ${productoAdicional}`);
+    cantidadAdicional = Number(prompt("Ingrese la cantidad deseada :"));
     let precioAdicional = precioBebida;
     precioProducto = precioBebida;
-    totalApagar = netoApagar + totalAdicional * precioAdicional;
+    totalAdicional = precioAdicional * cantidadAdicional;
+    console.log(
+      `Ud agregó ${cantidadAdicional} ${productoAdicional}. El monto adicional es de $ ${totalAdicional}`
+    );
+    totalApagar = netoApagar + totalAdicional;
     alert(`El monto a abonar es de $ ${totalApagar}`);
   }
   agregarPedido = confirm("¿Desea agreagar algun producto más?");
 }
 alert(
-  `Su pedido ${nombreUsuario} será enviado a la brevedad a ${direccionEnvio}. Muchas gracias por confiar en el sabor y calidad de Gennaros Pizzas & Empanadas`
+  `Su pedido ${nombreUsuario} será enviado a la brevedad a ${direccionEnvio}. El total a abonar es de $ ${totalApagar} Muchas gracias por confiar en el sabor y calidad de Gennaros Pizzas & Empanadas`
 );
